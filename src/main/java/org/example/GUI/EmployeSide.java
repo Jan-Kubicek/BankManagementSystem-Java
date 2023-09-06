f9a730e0d5dd1a69376e156ec8a3d46161fda783
Jan Kubíček <kubicja1@uhk.cz>
2023-09-06 11:49:25 +0200

    EmployeSide - Common GUI - Panels

--- src/main/java/org/example/GUI/EmployeSide.java
+++ src/main/java/org/example/GUI/EmployeSide.java
@@ -1,6 +1,7 @@
 package org.example.GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class EmployeSide extends JFrame {
     //Komponenty
@@ -17,6 +18,33 @@ public class EmployeSide extends JFrame {
     }
     //GUI
     public void InitGUI(){
+        //North
+        pnlNorth  = new JPanel(new GridLayout(1,4));
+
+        add(pnlNorth,BorderLayout.NORTH);
+        //East
+        pnlEast = new JPanel();
+            pnlEast.setBackground(Color.getColor("",ColorPalette.getSidePannels()));
+        add(pnlEast,BorderLayout.EAST);
+        //West
+        pnlWest = new JPanel();
+            pnlWest.setBackground(Color.getColor("",ColorPalette.getSidePannels()));
+        add(pnlWest,BorderLayout.WEST);
+        //Center
+        pnlCenter = new JPanel( new GridLayout(2,1));
+            //Upper Center
+            pnlUpperCenter = new JPanel(new GridLayout(2,2));
+            pnlCenter.add(pnlUpperCenter);
+            //Bottom Center
+            pnlBottomCenter = new JPanel(new BorderLayout());
+                //Bottom Center - Center
+                pnlBottomCenterCenter = new JPanel(new GridLayout(1,1));
+                pnlBottomCenter.add(pnlBottomCenterCenter,BorderLayout.CENTER);
+                //Bottom Center - East
+                pnlBottomCenterEast = new JPanel(new GridLayout(3,1));
+                pnlBottomCenter.add(pnlBottomCenterEast,BorderLayout.EAST);
+            pnlCenter.add(pnlBottomCenter);
+        add(pnlCenter,BorderLayout.CENTER);
 
         pack();
     }
